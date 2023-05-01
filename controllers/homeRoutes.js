82410fd3015f77653a64bda84bb88196130b9892
Alyssa Geria <alyssageria@gmail.com>
2023-05-01 11:18:27 -0700

    designed the home/login/signup pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -63,4 +63,14 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
+});
+
 module.exports = router;
