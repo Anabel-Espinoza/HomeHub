40f48a4444f105e019a4c652c0d8beb30122b778
Jaimon <jamin.hogan@gmail.com>
2023-05-02 22:33:59 -0400

    added bing streetview to landlord/properties

--- public/js/properties.js
+++ public/js/properties.js
@@ -1,5 +1,9 @@
+
 //function to generate a street view URL for use with Bing API
 function getStreetView(streetAddress) {
+  const bingAPIKey = 'AiG4EPc6Fx1YkYlJcKu0BI-b5jWafgdxk4pQkefyU5iNYFa2wn0x24qyz8v4BY1d';
+
+  // console.log("key is:", bingAPIKey);
   let formattedAddress = "";
   let tmpAddress = streetAddress.split(" ");
   for (let i = 0; i < tmpAddress.length; i++) {
@@ -8,13 +12,24 @@ function getStreetView(streetAddress) {
     } else {
       formattedAddress = formattedAddress.concat("%20", tmpAddress[i]);
     }
-    console.log(formattedAddress);
+    // console.log(formattedAddress);
   }
   const streetView = `https://dev.virtualearth.net/REST/v1/Imagery/Map/Streetside/${formattedAddress}?zoomlevel=0&key=${bingAPIKey}`;
   return streetView;
 }
 
 function createPropertyCards(){
+  // console.log("display bing:", `${bingMapString}`);
+  let addresses = document.getElementsByClassName("bing-address");
+  let address = '';
+  for (let i = 0; i < addresses.length; i++){
+    // address += addresses[i].data;
+    address = addresses[i].id;
+    // console.log("the current propery address is:", addresses[i].id);
+    let bingMapString = getStreetView(address);
+    // console.log(bingMapString);
+    addresses[i].setAttribute("src", bingMapString);
+  }
 
 }
 
@@ -100,14 +115,8 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
-
-
 // Populate images in modal cards when API available
-
-
 createPropertyCards();
-
 
 document.querySelector('.new-property-form').addEventListener('submit', newPropertyHandler)
 // module.exports = closeAllModals;
