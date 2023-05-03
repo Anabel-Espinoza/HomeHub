afe34b44a90d80482114ce87e5368ab4be527db7
Alyssa Geria <alyssageria@gmail.com>
2023-05-02 17:12:31 -0700

    made progress on unit tenant page by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,7 +26,7 @@ router.get('/tenant', withAuth, async (req, res) => {
     const unitData = await Unit.findOne({
       where: {
         tenant_id: req.session.tenant_id,
-        },
+      },
       // include: [{ model: Maintenance, where: { is_closed: false } }]
     });
     if (unitData) {
@@ -181,7 +181,8 @@ router.get('/tenant/unit/:id', withAuth, async (req, res) => {
         model: Tenant,
         attributes: { exclude: ['password'] }
       }, {
-        model: Maintenance, where: { tenant_id: req.session.tenant_id }}],
+        model: Maintenance, where: { tenant_id: req.session.tenant_id }
+      }],
     })
     const unit = unitById.get({ plain: true })
     console.log(unit)
