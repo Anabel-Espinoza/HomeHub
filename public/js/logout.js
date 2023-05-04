59af4cebd1dff1fefbd9151103e31bb3132bc205
Alyssa Geria <alyssageria@gmail.com>
2023-05-04 10:31:01 -0700

    navbar burger functionality/ fixed dashboard error

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
         headers: { 'Content-Type': 'application/json' },
     })
 
-    if(response.ok) {
+    if (response.ok) {
         document.location.replace('/')
     } else {
         alert(response.statusText)
@@ -14,11 +14,11 @@ const logout = async () => {
 
 document.querySelector('#logout').addEventListener('click', logout)
 
-const dashboard = document.querySelector('.dashboard')
-const landlord = window.location.toString().includes('landlord')
+// const dashboard = document.querySelector('.dashboard')
+// const landlord = window.location.toString().includes('landlord')
 
-if (landlord) {
-    dashboard.setAttribute('href', '/landlord')
-} else {
-    dashboard.setAttribute('href', '/tenant')
-}
+// if (landlord) {
+//     dashboard.setAttribute('href', '/landlord')
+// } else {
+//     dashboard.setAttribute('href', '/tenant')
+// }
