c2496fefe5aa9298eb624e56ae0a07db5a1750d0
Alyssa Geria <alyssageria@gmail.com>
2023-05-08 17:45:56 -0700

    cleaned up commented out code

--- controllers/api/logoutRoute.js
+++ controllers/api/logoutRoute.js
@@ -2,15 +2,8 @@ const router = require("express").Router();
 
 router.post('/', (req, res) => {
     req.session.destroy(() => {
-         res.status(204).end();
+        res.status(204).end();
     })
-    // if (req.session.logged_in) {
-    //     req.session.destroy(() => {
-    //         res.status(204).end();
-    //     });
-    // } else {
-    //     res.status(404).end();
-    // }
 });
 
 module.exports = router;
